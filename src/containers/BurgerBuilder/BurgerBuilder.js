c7982dac810311d235276ea034c23f3bb6ad64c7
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-01-30 16:06:47 +0530

    send post request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'; 
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICE = {
     salad: 0.3,
     cheese: 0.4,
@@ -78,7 +80,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert("purchase continued")
+        // alert("purchase continued")
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice.toFixed(2),
+            customer: {
+                name: 'sanket dhabarde',
+                address: {
+                    street:'test data',
+                    zipcode:'39843',
+                    country: 'india'
+                },
+                email:'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render(){
