0966184a21c6089e8b011ccb89dddfff0ec327a0
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-06-05 16:32:18 +0530

    feat: add signin

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,103 +1,134 @@
-import React, { Component } from 'react';
-import Input from '../../components/UI/Input/Input';
-import Button from '../../components/UI/Button/Button';
-import classes from './Auth.css'
+import React, { Component } from "react";
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+import classes from "./Auth.css";
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 
 class Auth extends Component {
-    state = {
-        controls: {
-            email: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'email',
-                    placeholder: 'Your Mail'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    isEmail:true
-                },
-                valid: false,
-                touched: false
-            },
-            password: {
-                elementType: 'input',
-                elementConfig: {
-                    type: 'password',
-                    placeholder: 'password'
-                },
-                value: '',
-                validation: {
-                    required: true,
-                    minLength: 6
-                },
-                valid: false,
-                touched: false
-            }
-        }
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your Mail",
+        },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: "input",
+        elementConfig: {
+          type: "password",
+          placeholder: "password",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
+    isSignUp: true,
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
     }
 
-    checkValidity(value, rules){
-        let isValid= true;
-
-        if(rules.required){
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if(rules.minLength){
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if(rules.maxLength){
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        return isValid;
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
     }
 
-    inputChangedHandler = (event, controlName) =>{
-       const updatedControls = {
-           ...this.state.controls,
-           [controlName]: {
-               ...this.state.controls[controlName],
-               value: event.target.value,
-               valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-               touched: true
-           }
-       }
-       this.setState({controls: updatedControls});
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
     }
+    return isValid;
+  }
 
-    render(){
-        const formElements=[];
-
-        for(let key in this.state.controls){
-            formElements.push({
-                id: key,
-                config: this.state.controls[key]
-            });
-        }
-
-        const form = formElements.map(formElement => (
-            <Input 
-                key={formElement.id}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-        ));
-
-        return (
-            <div>
-                <form className={classes.Auth}>
-                    {form}
-                    <Button btnType="Success">SUBMIT</Button>
-                </form>
-            </div>
-        );
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
     };
+    this.setState({ controls: updatedControls });
+  };
+
+  switchAuthModeHandler = () => {
+    this.setState((prevState) => {
+      return { isSignUp: !prevState.isSignUp };
+    });
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+      this.state.isSignUp
+    );
+  };
+
+  render() {
+    const formElements = [];
+
+    for (let key in this.state.controls) {
+      formElements.push({
+        id: key,
+        config: this.state.controls[key],
+      });
+    }
+
+    const form = formElements.map((formElement) => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+
+    return (
+      <div className={classes.Auth}>
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
+        <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+          SWITCH TO {this.state.isSignUp ? "SIGNIN" : "SIGNUP"}
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default Auth;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
